fd751ca39354ce476b7ec5492ec788d8096dbcb7
Kevin Bandison <kbandison@gmail.com>
2022-09-14 14:39:18 -0400

    completed what can yoou do

---
+++ what-can-you-do.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const prompt = require('prompt-sync')();
+
+let age = prompt('What is your age?');
+
+if (age < 16){
+  console.log("You can't drive.");
+} else if (age == 16 || age == 17){
+  console.log("You can drive but not vote.");
+} else if (age >= 18 && age <= 24) {
+  console.log("You can vote but not rent a car.");
+} else if (age >= 25){
+  console.log("You can do pretty much anything.");
+}
